bfbce50f3e2245c71e065a3ba3f6f8af169c5284
kminsic <ehdns9800@gmail.com>
2022-09-24 16:45:28 +0900

    레퍼지토리 추가

--- src/main/java/com/wak/igo/repository/PostRepository.java
+++ src/main/java/com/wak/igo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.wak.igo.repository;
 
+import com.wak.igo.domain.Member;
 import com.wak.igo.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,12 +11,10 @@ import java.util.Optional;
 @Repository
 public interface PostRepository  extends JpaRepository<Post,Long> {
 
-//    List<Post> findByTagContaining (String tag);
 
 
 //    List<Post> findAllByPostByCreatedAtDesc(); //최신순 정렬
 //    List<Post> findAllByPostByViewcountDesc(); //조회수순 정렬
     Optional<Post> findById(Long id);
-
-//    List<Post> findAllByMemberId(Long id);
+    List<Post> findAllByMember(Member member);
 }
