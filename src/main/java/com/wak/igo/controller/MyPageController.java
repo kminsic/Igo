1bba755cee4eb0ab857d9c7b4b32edb88635e9ce
kminsic <ehdns9800@gmail.com>
2022-09-29 21:25:40 +0900

    클라이언트 id변경 (프론트엔드)

--- src/main/java/com/wak/igo/controller/MyPageController.java
+++ src/main/java/com/wak/igo/controller/MyPageController.java
@@ -17,12 +17,12 @@ public class MyPageController {
     private final MyPageService myPageService;
 
     // 내가 쓴 post 불러오기
-    @RequestMapping(value = "/api/member/post", method = RequestMethod.GET)
+    @GetMapping (value = "/api/member/post")
     public ResponseDto<?> getPost(HttpServletRequest request) {
         return myPageService.getPost(request);
     }
     // 나의일정 불러오기
-    @RequestMapping(value = "/api/member/mypost", method = RequestMethod.GET)
+    @GetMapping(value = "/api/member/mypost")
     public ResponseDto<?> getMypost(HttpServletRequest request) {
         return myPageService.getMypost(request);
     }
@@ -37,8 +37,9 @@ public class MyPageController {
         return myPageService.getHeartpost(request);
     }
     //회원정보 수정하기
-    @PutMapping("api/auth/member/mypage")
-    public ResponseDto<?> updateMember(HttpServletRequest request,@RequestPart("profileimage") MultipartFile file,
+    @PutMapping (value = "/api/auth/member/mypage")
+    public ResponseDto<?> updateMember(HttpServletRequest request,
+                                       @RequestPart("profileimage") MultipartFile file,
                                        @RequestPart("nickname") MemberResponseDto memberResponseDto
                                        ) throws IOException {
         return myPageService.updateMember(request,file,memberResponseDto);
