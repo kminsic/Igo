59061c7c0aa30a80504b8db0ce3358a528328763
kminsic <110470208+kminsic@users.noreply.github.com>
2022-10-04 14:14:22 +0900

    Update health_check.sh

--- script/health_check.sh
+++ script/health_check.sh
@@ -17,12 +17,12 @@ else
 fi
 
 
-echo "> Start health check of WAS at 'http://3.36.78.242:${TARGET_PORT}' ..."
+echo "> Start health check of WAS at 'http://13.124.177.157:${TARGET_PORT}' ..."
 
 for RETRY_COUNT in 1 2 3 4 5
 do
     echo "> #${RETRY_COUNT} trying..."
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"  http://3.36.78.242:${TARGET_PORT}/health)
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"  http://13.124.177.157:${TARGET_PORT}/health)
 
     if [ ${RESPONSE_CODE} -eq 200 ]; then
         echo "> New WAS successfully running"
