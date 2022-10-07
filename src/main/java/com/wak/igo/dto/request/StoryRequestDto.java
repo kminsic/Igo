a8506d401d25369ae9e187912a13d90713226481
lee0119 <candogo91@gmail.com>
2022-10-07 16:52:13 +0900

    fix 수정

--- src/main/java/com/wak/igo/dto/request/StoryRequestDto.java
+++ src/main/java/com/wak/igo/dto/request/StoryRequestDto.java
@@ -2,13 +2,15 @@ package com.wak.igo.dto.request;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.NotNull;
 @Getter
 @Builder
 public class StoryRequestDto {
+    MultipartFile multipartFile;
+
         private Long member;
-        private String video;
+        private MultipartFile video;
     }
 
 
