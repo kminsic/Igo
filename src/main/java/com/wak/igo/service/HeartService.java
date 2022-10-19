627a680aee0ecf4861f5cbbc03de43d81254b19f
lee0119 <candogo91@gmail.com>
2022-10-19 18:26:01 +0900

    fix heart 수정

--- src/main/java/com/wak/igo/service/HeartService.java
+++ src/main/java/com/wak/igo/service/HeartService.java
@@ -30,13 +30,13 @@ public class HeartService {
             ResponseDto.fail("MEMBER_NOT_FOUND",
                     "사용자를 찾을 수 없습니다.");
         }
+
         Optional<Post> post = postRepository.findById(id);
-        Member postMember = post.get().getMember();
-        Post postNotification = postRepository.findByMemberId(userDetails.getId());
         if (post.isEmpty()) {
             return ResponseDto.fail("BAD_REQUEST", "해당 게시글이 존재하지 않습니다.)");
         }
-
+        Post postNotification = postRepository.findByMemberId(userDetails.getId());
+        Member postMember = post.get().getMember();
         Optional<Heart> heart = heartRepository.findByMemberIdAndPostId(userDetails.getId(), post.get().getId());
         if (heart.isEmpty()) {
             heartRepository.save(Heart.builder()
